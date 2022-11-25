e3fb2775b25e6dde87b738eacfc192df7459229c
RCT-Developer <rameshtulal@yahoo.com>
2022-11-25 16:19:13 +0530

    Reporting concept added

--- src/methods/Utilities.java
+++ src/methods/Utilities.java
@@ -3,5 +3,7 @@ package methods;
 public class Utilities {
 	//All utilities related methods will be added here in this class
 	//Thanks you, RCT
-
+public void utilitiesMethod1(){
+    //This method is to take care of the reporting
+}
 }
